12201ac826a67f456dd854c05d484eaef98877dc
Greg Eusden <gregeusden@MacBook-Pro-8.local>
2025-12-02 10:35:47 -0500

    Solved day 2

--- day_02/solution_1.rb
+++ day_02/solution_1.rb
@@ -1,9 +1,37 @@
 # Solution for Day 2
 
-require_relative '../utils'
+require "pry"
+require_relative "../utils"
 
 # Read input lines
 lines = read_input_lines("day_02/input.txt")
 
 # Solution logic
+#
+bad_line_array = []
 
+lines.each do |line|
+  ranges = line.split(",")
+  ranges.each do |range|
+    low, high = range.split("-").map(&:to_i)
+    for i in low..high
+      string_i = i.to_s
+      str_length = string_i.length
+      if str_length % 2 == 1
+        next
+      else
+        # TAke first half of the number and compare to second half
+        first_half = string_i[0..(str_length / 2 - 1)]
+        second_half = string_i[(str_length / 2)..-1]
+        # puts "First half: #{first_half}, Second half: #{second_half}, Match? #{first_half == second_half}"
+        if first_half == second_half
+          bad_line_array << i
+        end
+      end
+    end
+    puts "Completed range: #{range}"
+  end
+end
+
+# SUm the bad_line array
+puts bad_line_array.sum
--- day_02/solution_2.rb
+++ day_02/solution_2.rb
@@ -1,9 +1,40 @@
 # Solution for Day 2
 
-require_relative '../utils'
+require "pry"
+require_relative "../utils"
 
 # Read input lines
 lines = read_input_lines("day_02/input.txt")
 
 # Solution logic
+#
+bad_line_array = []
 
+lines.each do |line|
+  ranges = line.split(",")
+  ranges.each do |range|
+    low, high = range.split("-").map(&:to_i)
+    for i in low..high
+      string_i = i.to_s
+      str_length = string_i.length
+      # SEries of hardcoded given the lines dont get that long.
+      for repeat_length in 1..str_length / 2
+        if str_length % repeat_length == 0
+          repeats = str_length / repeat_length
+          # TAke first half of the number and compare to second half
+          first_third = string_i[0..repeat_length - 1]
+          new_string = first_third * repeats
+          if new_string == string_i
+            bad_line_array << i
+            break # break out of the for loop, this one matches.
+          end
+        end
+      end
+    end
+    puts "Completed range: #{range}"
+  end
+end
+
+# SUm the bad_line array
+# puts bad_line_array.join(", ")
+puts bad_line_array.sum
